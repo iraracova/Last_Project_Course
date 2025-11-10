f42429a90a4bf27e67a79a289ff08fd4525110aa
Ira Rakova <racova-ir@mail.ru>
2025-11-10 15:44:59 +0300

    add_method_exception

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,20 +1,20 @@
+import pytest
+from selenium import webdriver
+from .locators import MainPageLocators
 from .base_page import BasePage
 
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        current_url = driver.current_url
+        assert True "login" in current_url, "login is presented in current url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert True self.is_element_present(*LoginPageLocators.LOGIN_FORM), "login form is presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert True self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is presented"
