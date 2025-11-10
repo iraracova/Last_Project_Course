bcc9a2b2636918c33050bcee57cb4b98bf0c98d0
Ira Rakova <racova-ir@mail.ru>
2025-11-10 09:49:43 +0300

    add_changes_by_page_object

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    time.sleep(10)
+    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
+   
